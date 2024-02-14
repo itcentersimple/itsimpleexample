4dc68a7ce9668af2c4fd9a38ac88e5d55e7961f4
itcentersimple <159968336+itcentersimple@users.noreply.github.com>
2024-02-13 20:26:58 -0500

    example_3.py updated. submit added.

--- example_3.py
+++ example_3.py
@@ -4,3 +4,4 @@ import pytest
 def test_new(browser):
     browser.get("https://www.ebay.com")
     browser.find_element(By.NAME, "search").send_keys("something")
+    browser.find_element(By.NAME, "submit").click()
